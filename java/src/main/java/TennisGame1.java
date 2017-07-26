77ae80e485db9a22d8f9e02e3f933b9d5f1c69d0
tanapat.so <tanapat.soontornlimsiri@gmail.com>
2017-07-26 09:29:16 +0700

    Refactor tennisGame 1 case advantage and winner

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -5,7 +5,7 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
-    String[] scoreString = {"Love","Fifteen","Thirty","Forty","Deuce"};
+    String[] scoreString = {"Love","Fifteen","Thirty","Forty","Deuce","Advantage ","Win for "};
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -20,8 +20,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore = 0;
+
         //case Love-All and Deuce
         if (m_score1 == m_score2) {
             if (m_score1<3) {
@@ -33,17 +32,35 @@ public class TennisGame1 implements TennisGame {
         //case Advantage and Winner
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if (minusResult == 1)
-                score = "Advantage player1";
-            else if (minusResult == -1)
-                score = "Advantage player2";
-            else if (minusResult >= 2)
-                score = "Win for player1";
-            else
-                score = "Win for player2";
-        } else {
-                return scoreString[m_score1]+"-"+scoreString[m_score2];
+            if (IsAdvantage()){
+                return Advantage(minusResult);
+            }
+            else{
+                return Winner(minusResult);
+            }
         }
-        return score;
+        //Case Normal
+        return String.format("%s-%s", scoreString[m_score1], scoreString[m_score2]);
     }
+
+    public boolean IsAdvantage() {
+        if (Math.abs(m_score1-m_score2) ==1){
+            return true;
+        }
+        return false;
+    }
+    public String Advantage(int minusResult) {
+        if (minusResult == 1){
+            return String.format("%s%s",scoreString[5], player1Name);
+        }
+        return String.format("%s%s",scoreString[5], player2Name);
+    }
+
+    public String Winner(int minusResult) {
+        if (minusResult >= 2){
+            return String.format("%s%s",scoreString[6], player1Name);
+        }
+        return String.format("%s%s",scoreString[6], player2Name);
+    }
+
 }
